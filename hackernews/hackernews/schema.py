88af314300dd1bfc0c5eaf55aa52a34a9b1727be
plasx <plasx@live.com>
2019-05-02 10:25:30 -0400

    created relayconnection and relay mutation for links

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -1,15 +1,21 @@
 import graphene
 import links.schema
 import users.schema
+import links.schema_relay
 import graphene
 import graphql_jwt
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(users.schema.Mutation, links.schema.Mutation, links.schema_relay.RelayMutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(
+    users.schema.Query,
+    links.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType
+):
     pass
 
 
